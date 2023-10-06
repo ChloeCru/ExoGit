b134647d660e1ec0e5134dacba9162bed1ac1f4a
lenac <lenac@159.31.75.183>
2023-10-06 14:36:21 +0200

    hgvygv

--- Test1/src/test/Test40.java
+++ Test1/src/test/Test40.java
@@ -4,7 +4,7 @@ public class Test40 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello calculatrice");
+System.out.println("hello calculatrice hhgg");
 	}
 
 }
